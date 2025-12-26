c60921d031d7f58426e259d3a0a1188ebd07909e
Виктория <prokhorovav_2007@mail.ru>
2025-12-26 03:08:13 +0300

    Add files via upload

---
+++ app.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <locale.h>
+#include "array.h"
+
+int main() {
+    setlocale(LC_CTYPE, "RUS");
+
+    puts("Лабораторные работы 11, 14, 16\n");
+    puts("\n");
+
+    int size;
+    printf("Введите количество элементов массива: ");
+    scanf("%d", &size);
+    puts("\n");
+
+    printf("РАБОТА 11\n");
+    task11();
+    puts("\n");
+
+    printf("РАБОТА 14\n");
+    task14(size);
+    puts("\n");
+
+    printf("РАБОТА 16\n");
+    task16(size);
+
+    return 0;
+}
